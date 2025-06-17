709d4ff81615de5e5877eb0a1653700719ade66a
okonomi <okonomi@oknm.jp>
2025-06-17 23:02:19 +0900

    setup vitest

---
+++ src/rules/enforces-shorthand.test.ts
@@ -0,0 +1,41 @@
+import { createRuleTester } from "eslint-vitest-rule-tester"
+import { describe, expect, it } from "vitest"
+
+import rule from "./enforces-shorthand"
+
+describe("enforces-shorthand", () => {
+  const { valid, invalid } = createRuleTester({
+    name: "enforces-shorthand",
+    rule,
+    configs: {
+      languageOptions: {
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+      },
+    },
+  })
+
+  it("should not report valid identifiers", async () => {
+    const code = `
+      <div className="size-1" />
+    `
+    const { result } = await valid({ code })
+
+    expect(result.messages).toHaveLength(0)
+  })
+
+  it("should report invalid identifiers", async () => {
+    const code = `
+      <div className="w-1 h-1" />
+    `
+    const { result } = await invalid({
+      code,
+      errors: [{ messageId: "useShorthand" }],
+    })
+
+    expect(result.output).toMatchSnapshot()
+  })
+})
