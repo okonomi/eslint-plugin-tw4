a870551e1237f8b2250a8da6105d20da060e6bbf
okonomi <okonomi@oknm.jp>
2025-06-21 16:18:35 +0900

    refactor test

--- src/rules/enforces-shorthand.test.ts
+++ src/rules/enforces-shorthand.test.ts
@@ -4,6 +4,16 @@ import { describe, expect, it } from "vitest"
 
 import rule from "./enforces-shorthand"
 
+function generateError(classnames: string[], shorthand: string) {
+  return {
+    messageId: "useShorthand",
+    data: {
+      classnames: classnames.join(" "),
+      shorthand,
+    },
+  }
+}
+
 describe("enforces-shorthand", () => {
   const { valid, invalid } = createRuleTester({
     name: "enforces-shorthand",
@@ -34,66 +44,28 @@ describe("enforces-shorthand", () => {
         {
           code: `<div className="w-1 h-1" />`,
           output: `<div className="size-1" />`,
-          errors: [
-            {
-              messageId: "useShorthand",
-              data: {
-                shorthand: "size-1",
-                classnames: "w-1 h-1",
-              },
-            },
-          ],
-          message: 'Use shorthand "size-1" instead of class names "w-1 h-1".',
+          errors: [generateError(["w-1", "h-1"], "size-1")],
         },
         {
           code: `<div className="w-1 block h-1" />`,
           output: `<div className="size-1 block" />`,
-          errors: [
-            {
-              messageId: "useShorthand",
-              data: {
-                shorthand: "size-1",
-                classnames: "w-1 h-1",
-              },
-            },
-          ],
-          message: 'Use shorthand "size-1" instead of class names "w-1 h-1".',
+          errors: [generateError(["w-1", "h-1"], "size-1")],
         },
         {
           code: `<div className="w-2 h-2" />`,
           output: `<div className="size-2" />`,
-          errors: [
-            {
-              messageId: "useShorthand",
-              data: {
-                shorthand: "size-2",
-                classnames: "w-2 h-2",
-              },
-            },
-          ],
-          message: 'Use shorthand "size-2" instead of class names "w-2 h-2".',
+          errors: [generateError(["w-2", "h-2"], "size-2")],
         },
         {
           code: `<div className="w-auto h-auto" />`,
           output: `<div className="size-auto" />`,
-          errors: [
-            {
-              messageId: "useShorthand",
-              data: {
-                shorthand: "size-auto",
-                classnames: "w-auto h-auto",
-              },
-            },
-          ],
-          message:
-            'Use shorthand "size-auto" instead of class names "w-auto h-auto".',
+          errors: [generateError(["w-auto", "h-auto"], "size-auto")],
         },
       ])(
         "should report invalid code: $code",
-        async ({ code, output, errors, message }) => {
+        async ({ code, output, errors }) => {
           const { result } = await invalid({ code, output, errors })
           expect(result.output).toEqual(output)
-          expect(result.messages[0].message).toEqual(message)
         },
       )
     })
